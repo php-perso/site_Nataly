db596676539228d602861818fc20c74cb83476ed
MParisNRX <michael.paris@nrx.eu>
2018-11-05 18:19:00 +0100

    avancement

--- src/Site/NatalyBundle/Controller/MainController.php
+++ src/Site/NatalyBundle/Controller/MainController.php
@@ -10,6 +10,36 @@ class MainController extends Controller
     {
         return $this->render('@SiteNataly/Main/index.html.twig');
     }
+
+    public function about_meAction()
+    {
+        return $this->render('@SiteNataly/Main/about_me.html.twig');
+    }
+
+    /*public function indexAction()
+    {
+        return $this->render('@SiteNataly/Main/index.html.twig');
+    }
+
+    public function indexAction()
+    {
+        return $this->render('@SiteNataly/Main/index.html.twig');
+    }
+
+    public function indexAction()
+    {
+        return $this->render('@SiteNataly/Main/index.html.twig');
+    }
+
+    public function indexAction()
+    {
+        return $this->render('@SiteNataly/Main/index.html.twig');
+    }
+
+    public function indexAction()
+    {
+        return $this->render('@SiteNataly/Main/index.html.twig');
+    }*/
 }
 
 
